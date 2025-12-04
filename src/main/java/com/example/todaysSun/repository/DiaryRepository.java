2429487c6b3d45f8295f00d6e674b958bb0a5b08
yeonseong0224 <kimyeonseong0224.4@gmail.com>
2025-12-05 03:55:17 +0900

    h2 - >postgreSQL 전용으로 데이터 저장 코드 바꿈

--- src/main/java/com/example/todaysSun/repository/DiaryRepository.java
+++ src/main/java/com/example/todaysSun/repository/DiaryRepository.java
@@ -16,8 +16,8 @@ public interface DiaryRepository extends JpaRepository<Diary, Long> {
 
     List<Diary> findAllByDate(LocalDate date);
 
-    // ✅ 연도만 중복 없이 가져오기
-    @Query("SELECT DISTINCT YEAR(d.date) FROM Diary d ORDER BY YEAR(d.date)")
+    // ✅ 연도만 중복 없이 가져오기 (PostgreSQL용)
+    @Query("SELECT DISTINCT EXTRACT(YEAR FROM d.date) FROM Diary d ORDER BY EXTRACT(YEAR FROM d.date)")
     List<Integer> findDistinctYears();
 
     Optional<Diary> findByDate(LocalDate date);
